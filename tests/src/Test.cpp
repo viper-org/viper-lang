22641f3bdf25a32abcc21b4879f43168fb7a7e8f
solar-mist <solarmistreal@gmail.com>
2023-07-06 23:06:11 +0000

    Fix typo

--- tests/src/Test.cpp
+++ tests/src/Test.cpp
@@ -43,7 +43,7 @@ void diagnostics(OutputType outputType)
     if(outputType == OutputType::Markdown)
     {
         std::cout << "# Test Results";
-        std::cout << "\n" << tests.size() << "tests run. :heavy_check_mark: " << tests.size() - failedTests.size() << " succeeded, :x: " << failedTests.size() << " failed.\n\n";
+        std::cout << "\n" << tests.size() << " tests run. :heavy_check_mark: " << tests.size() - failedTests.size() << " succeeded, :x: " << failedTests.size() << " failed.\n\n";
         for (failedTest& test : failedTests)
         {
             std::cout << "<details>\n\t<summary>:x:" << test.suite << "::" << test.name << "</summary>\n";
